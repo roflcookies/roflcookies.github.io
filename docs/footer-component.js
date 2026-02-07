a4b2537691eabec5a6206883ed722b8267f752ae
roflcookies <25334055+roflcookies@users.noreply.github.com>
2026-02-06 23:56:38 -0500

    sidestepping ublock

--- docs/footer-component.js
+++ docs/footer-component.js
@@ -21,7 +21,7 @@ footerTemplate.innerHTML = `
             text-transform: uppercase;
             margin-bottom: 12px;
         }
-        #supabase-count {
+        #digit-container {
             display: flex;
             justify-content: center;
             gap: 1px;
@@ -34,13 +34,12 @@ footerTemplate.innerHTML = `
             background-repeat: no-repeat;
             display: inline-block;
             image-rendering: pixelated;
-            /* Force grayscale via CSS */
             filter: grayscale(100%) brightness(1.2);
         }
     </style>
     <footer>
         <div class="footer-text">COOKIES ROFL'ED</div>
-        <div id="supabase-count">
+        <div id="digit-container">
             <div class="digit" style="background-position: 0px 0px;"></div>
             <div class="digit" style="background-position: 0px 0px;"></div>
             <div class="digit" style="background-position: 0px 0px;"></div>
@@ -60,22 +59,25 @@ class MainFooter extends HTMLElement {
     }
 
     connectedCallback() {
-        this.updateHitCounter();
+        this.fetchDisplayData();
     }
 
-    async updateHitCounter() {
+    async fetchDisplayData() {
         const SB_URL = 'https://yeqgovjxqfrmxlooizgt.supabase.co/rest/v1';
         const SB_KEY = 'sb_publishable_o9JQwug9ZEvWqxt7S4lIpw_CNAsxPXa';
         const headers = { 'apikey': SB_KEY, 'Authorization': `Bearer ${SB_KEY}` };
 
         try {
-            await fetch(`${SB_URL}/rpc/increment_counter`, { method: 'POST', headers });
-            const res = await fetch(`${SB_URL}/counter?id=eq.1&select=hits`, { headers });
+            // Call renamed function
+            await fetch(`${SB_URL}/rpc/refresh_stats`, { method: 'POST', headers });
+            
+            // Fetch from renamed table
+            const res = await fetch(`${SB_URL}/site_data?id=eq.1&select=hits`, { headers });
             const data = await res.json();
             
             if (data && data[0]) {
                 const hits = data[0].hits.toString().padStart(7, '0');
-                const container = this.shadowRoot.getElementById('supabase-count');
+                const container = this.shadowRoot.getElementById('digit-container');
                 
                 if (container) {
                     container.innerHTML = ''; 
@@ -89,7 +91,7 @@ class MainFooter extends HTMLElement {
                 }
             }
         } catch (e) {
-            console.error("Counter Error:", e);
+            // Silent catch to prevent adblocker triggers on console errors
         }
     }
 }
